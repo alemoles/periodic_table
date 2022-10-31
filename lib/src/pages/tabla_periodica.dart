c8de4123d4f1d4b5e4ba3935c8a1b7664d872bba
alemoles <alemolerio@gmail.com>
2022-10-30 23:02:22 -0300

    code refactor

--- lib/src/pages/tabla_periodica.dart
+++ lib/src/pages/tabla_periodica.dart
@@ -1,11 +1,7 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:periodic_table/src/models/data_layout.dart';
 import 'package:periodic_table/src/models/row_data.dart';
-import 'package:periodic_table/src/utils/utils.dart';
-import 'package:periodic_table/src/widgets/elemento.dart';
+import 'package:periodic_table/src/widgets/card_element.dart';
 import 'package:periodic_table/src/widgets/flip_card.dart';
 import 'package:provider/provider.dart';
 
@@ -61,76 +57,6 @@ class _TablaPeriodicaPageState extends State<TablaPeriodicaPage> {
               ],
             )
           : const CircularProgressIndicator.adaptive(),
-    );
-  }
-}
-
-class CardElement extends StatelessWidget {
-  const CardElement({
-    Key? key,
-    required this.controller,
-    required this.data,
-  }) : super(key: key);
-
-  final FlipCardController controller;
-  final RowData data;
-
-  @override
-  Widget build(BuildContext context) {
-    final symbols = data.symbolsUrl.map(
-      (url) => _Symbol(
-        url: url,
-      ),
-    );
-    return GestureDetector(
-      onTap: () => controller.flipCard(),
-      child: FlipCardWidget(
-        controller: controller,
-        front: Elemento(
-          atomicNumber: data.atomicNumber,
-          atomicSymbol: data.atomicSymbol,
-          name: data.name,
-          widget: data.widget,
-          image: SvgPicture.asset(
-            data.imageUrl,
-          ),
-          symbols: [
-            ...symbols,
-          ],
-          color: fromName(
-            data.type,
-          ),
-        ),
-        back: ElementBack(
-          descripcion: data.descripcion,
-          color: fromName(
-            data.type,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _Symbol extends StatelessWidget {
-  const _Symbol({
-    Key? key,
-    required this.url,
-  }) : super(key: key);
-  final String url;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 1,
-      ),
-      child: SizedBox(
-        height: 15,
-        width: 15,
-        child: SvgPicture.asset(
-          url,
-        ),
-      ),
     );
   }
 }
--- lib/src/widgets/elemento.dart
+++ lib/src/widgets/elemento.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Elemento extends StatelessWidget {
@@ -131,14 +130,7 @@ class _ElementoHeader extends StatelessWidget {
           ),
           Row(
             children: [
-              ...symbols
-              // Container(
-              //   height: 15,
-              //   width: 15,
-              //   child: SvgPicture.asset(
-              //     'assets/svgs/person.svg',
-              //   ),
-              // ),
+              ...symbols             
             ],
           ),
           Padding(
